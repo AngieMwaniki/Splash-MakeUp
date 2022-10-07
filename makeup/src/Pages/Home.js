1e2d0176f47a525f37a659419e64dd807391bdb8
AngieMwaniki <angela.mwaniki@student.moringaschool.com>
2022-10-07 15:57:40 +0300

    changes

--- makeup/src/Pages/Home.js
+++ makeup/src/Pages/Home.js
@@ -2,7 +2,7 @@
  import {Box} from '@mui/material'
  import HeroBanner from '../Components/HeroBanner'
  import SearchProducts from '../Components/SearchProducts'
- import Products from '../Components/Products'
+ import Produce from '../Components/Produce'
  
  const Home =() => {
    return (
