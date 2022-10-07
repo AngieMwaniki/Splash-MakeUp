6f64f8c7f9ebdc6cc482d19e161792191786f794
AngieMwaniki <angela.mwaniki@student.moringaschool.com>
2022-10-07 17:07:19 +0300

    changes

--- makeup/src/Components/Produce.js
+++ makeup/src/Components/Produce.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import Pagination from '@muimaterial/Pagination';
+import {Box,Stack,Typography} from '@mui/material'
+
 function Produce() {
   return (
     
