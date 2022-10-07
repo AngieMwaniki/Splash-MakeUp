307c65da8a37a05496bfb9f36e9bb62775ca2ab8
AngieMwaniki <angela.mwaniki@student.moringaschool.com>
2022-10-07 13:05:39 +0300

    add button

--- makeup/src/Components/SearchProducts.js
+++ makeup/src/Components/SearchProducts.js
@@ -1,5 +1,5 @@
 import { Typography } from '@mui/material'
-import { Stack } from '@mui/system';
+import { borderRadius, Stack } from '@mui/system';
 import React from 'react'
 import {Box, Button, TextField} from '@mui/material';
 
@@ -11,11 +11,32 @@ const SearchProducts = () => {
             Awesome Products You <br/> should know
         </Typography>
     <Box position="relative"  mb="72px" >
-       <TextField height="76px"
+       <TextField 
+       sx={{input:{
+        fontWeight: '700',
+       border:'none',
+       borderRadius: '4px'},
+       width: {lg: '1170px', xs:'350px'},
+       backgroundcolor:'#FFF',
+    borderRadius: '40PX'
+    }}
+       height="76px"
        value=""
        onChange={(e)=> {}}
+       placeholder="Search Products" type="text"
        />
     </Box>
+<Button cClassName="search-btn"
+sx={{
+    bgcolor:'#ff2625',
+    color:'#fff',
+    textTransform:'none',
+    width:{lg:'175px', xs: '80px'},
+    fontSize:{lg:'20px', xs:'14px'},
+    height: '56px',
+    position:"center"}}>
+Search
+</Button>
 
     </Stack>
   )
