04bc3a54e02a39e8002946ac6db2426950015674
AngieMwaniki <angela.mwaniki@student.moringaschool.com>
2022-10-07 12:27:15 +0300

    commit

--- makeup/src/Components/SearchProducts.js
+++ makeup/src/Components/SearchProducts.js
@@ -1,8 +1,20 @@
+import { Typography } from '@mui/material'
+import { Stack } from '@mui/system';
 import React from 'react'
+import {Box, Button, TextField} from '@mui/material';
 
-function SearchProducts() {
+const SearchProducts = () => {
   return (
-    <div>SearchProducts</div>
+    <Stack alignItems='center' mt="37px"
+    justifyContent="center" p="20px">
+        <Typography fontWeight={700} textAlign="center">
+            Awesome Products You <br/> should know
+        </Typography>
+    <Box>
+        
+    </Box>
+
+    </Stack>
   )
 }
 
